18996e29181eca68848ac636736cbe8c686a9f01
Flavio Mendes <flavio.mendes@workday.com>
2024-09-16 14:50:31 +0100

    adding place rating

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  
+  <React.StrictMode>
     <BrowserRouter>
       <Auth0Provider
         domain={import.meta.env.VITE_AUTH0_DOMAIN}
@@ -17,5 +17,5 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
         <App />
       </Auth0Provider>
     </BrowserRouter>
-  
+  </React.StrictMode>
 );
